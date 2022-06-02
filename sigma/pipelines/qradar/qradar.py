e8f1a3b15d80f50151733bfa46f98640f69391fa
nNipsx <86789668+nNipsx-Sec@users.noreply.github.com>
2022-06-02 16:41:53 +0700

    Update field mapping in Pipelines

--- sigma/pipelines/qradar/qradar.py
+++ sigma/pipelines/qradar/qradar.py
@@ -3,88 +3,228 @@ from sigma.processing.transformations import FieldMappingTransformation, Detecti
 from sigma.processing.conditions import LogsourceCondition, IncludeFieldCondition, ExcludeFieldCondition, RuleProcessingItemAppliedCondition, RuleContainsDetectionItemCondition 
 from sigma.processing.pipeline import ProcessingItem, ProcessingPipeline
 
-
+"""
+List Windows Field not support:AccessList
+Action
+Address
+AllowedToDelegateTo
+AttributeLDAPDisplayName
+AttributeValue
+AuditPolicyChanges
+AuditSourceName
+CallTrace
+Caption
+CertThumbprint
+Channel
+ClassName
+Company
+ContextInfo
+Description
+DestinationIsIpv6
+Device
+DeviceDescription
+DeviceName
+EngineVersion
+EventType
+FileVersion
+HiveName
+HostVersion
+Initiated
+KeyLength
+LayerRTID
+Level
+LocalName
+LogonId
+LogonProcessName
+NewName
+PipeName
+ParentUser
+PasswordLastSet
+PossibleCause
+PrivilegeList
+Product
+Properties
+Provider
+ProviderName
+Provider_Name
+QNAME
+Query
+QueryName
+QueryResults
+QueryStatus
+RelativeTargetName
+RemoteAddress
+Service
+SearchFilter
+ServerName
+ServicePrincipalNames
+ServiceStartType
+ServiceType
+SidHistory
+Signed
+Source_Name
+StartAddress
+StartFunction
+StartModule
+State
+Status
+SubjectDomainName
+SubjectLogonId
+SubjectUserSid
+TargetLogonId
+TargetName
+TicketEncryptionType
+TicketOptions
+Type
+User
+UserName
+Value
+Workstation
+WorkstationName
+TargetUserSid
+TargetSid
+ObjectServer
+ObjectClass
+OldUacValue
+Origin
+ObjectValueName
+OldTargetUserName
+DestinationHostname
+"""
 
 qradar_windows_event = {
-  "event_id": "EventID",
-  "EventID": "EventID",
-  "dst": "destinationip",
-  "dst_ip": "destinationip",
-  "src": "sourceip",
-  "src_ip": "sourceip",
-  "c-ip": "sourceip",
-  "cs-ip": "sourceip",
-  "c-uri": "URL",
-  "c-uri-extension": "URL",
-  "c-useragent": "user_agent",
-  "c-uri-query": "uri_query",
-  "cs-method": "Method",
-  "r-dns": "FQDN",
-  "ClientIP": "sourceip",
-  "ServiceFileName": "Service FileName",
-  "event_data.CommandLine": "Process CommandLine",
-  "CommandLine": "Process CommandLine",
-  "file_hash": "File Hash",
-  "hash": "File Hash",
-  "Event-ID": "EventID",
-  "Event_ID": "EventID",
-  "eventId": "EventID",
-  "event-id": "EventID",
-  "eventid": "EventID",
-  "hashes": "File Hash",
-  "url.query": "URL",
-  "resource.URL": "URL",
-  "event_data.CallingProcessName": "CallingProcessName",
-  "event_data.ComputerName": "Hostname/HOSTNAME",
-  "ComputerName": "Hostname/HOSTNAME",
-  "event_data.DestinationHostname": "Hostname/HOSTNAME",
-  "DestinationHostname": "Hostname/HOSTNAME",
-  "event_data.DestinationIp": "destinationip",
-  "event_data.DestinationPort": "destinationip",
-  "event_data.Details": "Target Details",
-  "Details": "Target Details",
-  "event_data.FileName": "Filename",
-  "event_data.Hashes": "File Hash",
-  "Hashes": "File Hash",
-  "event_data.Image": "Image",
-  "event_data.ImageLoaded": "LoadedImage",
-  "event_data.ImagePath": "SourceImage",
-  "ImagePath": "Image",
-  "Image":"Image",
-  "event_data.Imphash": "IMP Hash",
-  "Imphash": "IMP Hash",
-  "event_data.ParentCommandLine": "ParentCommandLine",
-  "ParentCommandLine": "ParentCommandLine",
-  "event_data.ParentImage": "ParentImage",
-  "ParentImage": "ParentImage",
-  "event_data.ParentProcessName": "ParentImageName",
-  "event_data.Path": "File Path",
-  "Path": "File Path",
-  "event_data.PipeName": "PipeName",
-  "event_data.ProcessCommandLine": "Process CommandLine",
-  "ProcessCommandLine": "Process CommandLine",
-  "event_data.ServiceFileName": "ServiceFileName",
-  "event_data.ShareName": "ShareName",
-  "event_data.Signature": "Signature",
-  "event_data.SourceImage": "SourceImage",
-  "event_data.StartModule": "StartModule",
-  "event_data.SubjectUserName": "username",
-  "event_data.SubjectUserSid": "SubjectUserSid",
-  "event_data.TargetFilename": "Filename",
-  "TargetFilename": "Filename",
-  "event_data.TargetImage": "TargetImage",
-  "TargetImage": "TargetImage",
-  "event_data.TicketOptions": "TicketOptions",
-  "event_data.User": "username",
-  "User": "username",
-  "user": "username",
-  "OriginalFileName": "OriginalFileName",
-  "ProcessId": "Process Id",
-  "ParentProcessId": "Parent Process ID",
-  "md5": "MD5 Hash",
-  "sha1": "SHA1 Hash",
-  "sha256": "SHA256 Hash"
+    "AccessMask": "Access Mask",
+    "Accesses": "Accesses",
+    "AccountName": "Username",
+    "Account_Name": "Username",
+    "ApplicationPath": "File Path",
+    "AuthenticationPackageName": "Authentication Package ",
+    "CallerProcessName": "Process Path",
+    "CommandLine": "Process CommandLine",
+    "Commandline": "Process CommandLine",
+    "Computer": "Machine ID",
+    "ComputerName": "Computer Name",
+    "CurrentDirectory": "Process Path",
+    "DestAddress": "Destination IP",
+    "DestPort": "Destination Port",
+    "Destination": "Destination IP",
+    "DestinationIp": "Destination IP",
+    "DestinationPort": "Destination Port",
+    "Details": "Target Details",
+    "ErrorCode": "Error Code",
+    "EventID": "EventID",
+    "FailureCode": "Error Code",
+    "FileName": "Filename",
+    "GrantedAccess": "Granted Access",
+    "Hashes": "File Hash",
+    "HostApplication": "Process Path",
+    "HostName": "HostName",
+    "Image ": "Image",
+    "ImageLoaded": "LoadedImage",
+    "ImagePath": "Image",
+    "Imphash": "IMP Hash",
+    "IntegrityLevel": "Integrity Level",
+    "IpAddress": "Source IP",
+    "Keywords": "Payload",
+    "LogonType": "Logon Type",
+    "MachineName": "Computer Name",
+    "Message": "Message",
+    "NewTargetUserName": "Target User Name",
+    "ObjectName": "ObjectName",
+    "ObjectType": "ObjectType ",
+    "OriginalFileName": "ImageName",
+    "OriginalFilename": "ImageName",
+    "OriginalName": "ImageName",
+    "ParentCommandLine": "ParentCommandLine",
+    "ParentImage": "ParentImage",
+    "Path": "File Path",
+    "Payload": "Process CommandLine",
+    "ProcessId": "Process Id",
+    "ProcessName": "Process Name",
+    "SamAccountName": "SAM Account Name",
+    "ScriptBlockText": "Process CommandLine ",
+    "ServiceFileName": "Service FileName",
+    "ServiceName": "Service Name ",
+    "ShareName": "ShareName",
+    "SourceAddress": "Source IP",
+    "SourceImage": "SourceImage",
+    "SourcePort": "Source Port",
+    "SubjectUserName": "AccountName ",
+    "TargetFilename": "Filename",
+    "TargetImage": "TargetImage",
+    "TargetObject": "Target Object",
+    "TargetPort": "Destination Port",
+    "TargetServerName": "Destination IP",
+    "TargetUserName": "Target User Name",
+    "TaskName": "Task Name ",
+    "param1": "Payload",
+    "param2": "Payload",
+    "processPath": "Process Path",
+    "sha1": "SHA1 Hash",
+    "event_id": "EventID",
+    "dst": "destinationip",
+    "dst_ip": "destinationip",
+    "src": "sourceip",
+    "src_ip": "sourceip",
+    "c-ip": "sourceip",
+    "cs-ip": "sourceip",
+    "c-uri": "URL",
+    "c-uri-extension": "URL",
+    "c-useragent": "user_agent",
+    "c-uri-query": "uri_query",
+    "cs-method": "Method",
+    "r-dns": "FQDN",
+    "ClientIP": "sourceip",
+    "event_data.CommandLine": "Process CommandLine",
+    "file_hash": "File Hash",
+    "hash": "File Hash",
+    "Event-ID": "EventID",
+    "Event_ID": "EventID",
+    "eventId": "EventID",
+    "event-id": "EventID",
+    "eventid": "EventID",
+    "hashes": "File Hash",
+    "url.query": "URL",
+    "resource.URL": "URL",
+    "event_data.CallingProcessName": "CallingProcessName",
+    "event_data.ComputerName": "Hostname/HOSTNAME",
+    "event_data.DestinationHostname": "Hostname/HOSTNAME",
+    "event_data.DestinationIp": "destinationip",
+    "event_data.DestinationPort": "destinationip",
+    "event_data.Details": "Target Details",
+    "event_data.FileName": "Filename",
+    "event_data.Hashes": "File Hash",
+    "event_data.Image": "Image",
+    "event_data.ImageLoaded": "LoadedImage",
+    "event_data.ImagePath": "SourceImage",
+    "Image": "Image",
+    "event_data.Imphash": "IMP Hash",
+    "event_data.ParentCommandLine": "ParentCommandLine",
+    "event_data.ParentImage": "ParentImage",
+    "event_data.ParentProcessName": "ParentImageName",
+    "event_data.Path": "File Path",
+    "event_data.PipeName": "PipeName",
+    "event_data.ProcessCommandLine": "Process CommandLine",
+    "ProcessCommandLine": "Process CommandLine",
+    "event_data.ServiceFileName": "ServiceFileName",
+    "event_data.ShareName": "ShareName",
+    "event_data.Signature": "Signature",
+    "event_data.SourceImage": "SourceImage",
+    "event_data.StartModule": "StartModule",
+    "event_data.SubjectUserName": "username",
+    "event_data.SubjectUserSid": "SubjectUserSid",
+    "event_data.TargetFilename": "Filename",
+    "event_data.TargetImage": "TargetImage",
+    "event_data.TicketOptions": "TicketOptions",
+    "event_data.User": "username",
+    "User": "username",
+    "user": "username",
+    "ParentProcessId": "Parent Process ID",
+    "md5": "MD5 Hash",
+    "sha256": "SHA256 Hash",
 }
+
+
 
 qradar_windows_qid = {
     4688: [5001828, 5000862],
@@ -92,11 +232,14 @@ qradar_windows_qid = {
     4625: [5000475],
     4719: [5000891],
     4739: [5000909],
+    1102: [5001534],
     4698: [5000872],
     4699: [5000873],
     4700: [5000874],
     4701: [5000875],
     4702: [5000876],
+    4720: [5000892],
+    4722: [5000893],
     4723: [5000894],
     4724: [5002722],
     4728: [5000899],
@@ -104,6 +247,10 @@ qradar_windows_qid = {
     4768: [5000937],
     4769: [5000434],
     4770: [5000939],
+    5140: [5001108],
+    5142: [5002951],
+    5143: [5001562],
+    5144: [5001529],
     7045: [5002197],
     104: [5001534],
     521: [5001513],
@@ -111,7 +258,28 @@ qradar_windows_qid = {
     1100: [5001594],
     1104: [5002616],
     1108: [5002390],
-    1: [5001828, 5000862]
+    1: [5001828, 5000862],
+    2: [5001838],
+    3: [5001840],
+    4: [5001841],
+    5: [5001842],
+    6: [5001843],
+    7: [5001844],
+    8: [5001845],
+    9: [5001846],
+    10: [5001829],
+    11: [5001830],
+    12: [5001831],
+    13: [5001832],
+    14: [5001833],
+    15: [5001834],
+    16: [5001835],
+    17: [5001836],
+    18: [5001837],
+    19: [5001977],
+    20: [5001978],
+    21: [5001979],
+    22: [5002343]
 
 }
 qradar_logsource_id ={
@@ -127,7 +295,7 @@ def qradar_windows_events_acceleration_keywords():
         items= [
             ProcessingItem(
                 identifier="Qradar_savedsearches_unsupported_fields",
-                transformation=DetectionItemFailureTransformation("The QRadar savedsearches Sigma backend supports only the following fields for process_creation log source: " + ",".join(qradar_windows_event.keys())),
+                transformation=DetectionItemFailureTransformation("The QRadar savedsearches Sigma backend supports only the following fields for process_creation log source"),
                 rule_conditions=[
                     LogsourceCondition(
                         product="windows"
--- tests/test_qradar_pipelines.py
+++ tests/test_qradar_pipelines.py
@@ -42,7 +42,8 @@ def test_qradar_pipeline_process_creation_field_mapping():
                     sha256: 'Let it Bleed'
                 condition: sel
         """),"savedsearches"
-    ) == 'SELECT UTF8(payload) as search_payload from events where "Process Id"=1962 AND "Image"=\'Paint it Black\' AND "OriginalFileName"=\'Wild Horses\' AND "Process CommandLine"=\'Jumpin\' Jack Flash\' AND "username"=\'Mick Jagger\' AND "Parent Process ID"=1972 AND "ParentImage"=\'Muddy Waters\' AND "MD5 Hash"=\'Steel Wheels\' AND "SHA1 Hash"=\'Beggars Banquet\' AND "SHA256 Hash"=\'Let it Bleed\''
+    ) == 'SELECT UTF8(payload) as search_payload from events where "Process Id"=1962 AND "Image"=\'Paint it Black\' AND "ImageName"=\'Wild Horses\' AND "Process CommandLine"=\'Jumpin\' Jack Flash\' AND "username"=\'Mick Jagger\' AND "Parent Process ID"=1972 AND "ParentImage"=\'Muddy Waters\' AND "MD5 Hash"=\'Steel Wheels\' AND "SHA1 Hash"=\'Beggars Banquet\' AND "SHA256 Hash"=\'Let it Bleed\''
+
 
 def test_qradar_pipeline_dns_field_mapping():
     assert QradarBackend().convert(
@@ -81,8 +82,8 @@ def test_qradar_pipeline_web_proxy_field_mapping():
     ) == ['"c-uri"=\'https://www.thebeatles.com/\' AND "c-uri-query"=\'songs\' AND "cs-method"=\'GET\' AND "r-dns"=\'www.thebeatles.com\' AND INCIDR(\'192.168.1.0/24\', "src_ip") AND "dst_ip"=\'54.229.169.162\'']
 
 
-def test_insight_idr_pipeline_unsupported_field_process_start():
-    with pytest.raises(SigmaTransformationError, match="The QRadar savedsearches Sigma backend supports only the following fields for process_creation log source: event_id,EventID,dst,dst_ip,src,src_ip,c-ip,cs-ip,c-uri,c-uri-extension,c-useragent,c-uri-query,cs-method,r-dns,ClientIP,ServiceFileName,event_data.CommandLine,CommandLine,file_hash,hash,Event-ID,Event_ID,eventId,event-id,eventid,hashes,url.query,resource.URL,event_data.CallingProcessName,event_data.ComputerName,ComputerName,event_data.DestinationHostname,DestinationHostname,event_data.DestinationIp,event_data.DestinationPort,event_data.Details,Details,event_data.FileName,event_data.Hashes,Hashes,event_data.Image,event_data.ImageLoaded,event_data.ImagePath,ImagePath,Image,event_data.Imphash,Imphash,event_data.ParentCommandLine,ParentCommandLine,event_data.ParentImage,ParentImage,event_data.ParentProcessName,event_data.Path,Path,event_data.PipeName,event_data.ProcessCommandLine,ProcessCommandLine,event_data.ServiceFileName,event_data.ShareName,event_data.Signature,event_data.SourceImage,event_data.StartModule,event_data.SubjectUserName,event_data.SubjectUserSid,event_data.TargetFilename,TargetFilename,event_data.TargetImage,TargetImage,event_data.TicketOptions,event_data.User,User,user,OriginalFileName,ProcessId,ParentProcessId,md5,sha1,sha256"):
+def test_qradar_pipeline_unsupported_field_process_start():
+    with pytest.raises(SigmaTransformationError, match="The QRadar savedsearches Sigma backend supports only the following fields for process_creation log source"):
         QradarBackend().convert(
             SigmaCollection.from_yaml("""
                 title: Test
